758551491f6e93f9b127e0cd3b71711b5d51c1ea
SayakSR <sayaksr05@gmai..com>
2022-09-19 02:44:38 +0530

    new banner

--- run_stf.py
+++ run_stf.py
@@ -4,7 +4,7 @@ from stf_main_driver import *
 def run(): # Runs Social ThreatFinder
 
     from pyfiglet import Figlet
-    custom_fig = Figlet(font='mini')
+    custom_fig = Figlet(font='contessa')
     print(custom_fig.renderText('Social ThreatFinder'))
 
     while 1: 
